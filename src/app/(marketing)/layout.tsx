89ea8ecee2ca3e82d424806550a707ae6b1777a4
tsmialek <tsmialek.1@gmail.com>
2026-02-22 23:52:34 +0100

    feat(auth): Add custom sign-in/sign-up Clerk components
    - switch to webpack instead of turbopack
    - add `.env.example`
    - code cleanup

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,14 +1,11 @@
 import { ReactNode } from 'react';
 import { NavBar } from './_components/NavBar';
-import { ClerkProvider } from '@clerk/nextjs';
 
 export default function MarketingLayout({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
-      <div className='selection:bg-[hsl(320,65%,52%,20%)]'>
-        <NavBar />
-        {children}
-      </div>
-    </ClerkProvider>
+    <div className='selection:bg-[hsl(320,65%,52%,20%)]'>
+      <NavBar />
+      {children}
+    </div>
   );
 }
